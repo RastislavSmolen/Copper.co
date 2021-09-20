6e3c651ba5a94aacfad1735bdad51cf0a5a9eb13
Rastislav Smolen <>
2021-09-20 02:00:57 +0100

    displaying full data with no lag with correct date formatter

--- Copper.co/Copper.co/DisplayOrdersViewController/DisplayOrdersViewController.swift
+++ Copper.co/Copper.co/DisplayOrdersViewController/DisplayOrdersViewController.swift
@@ -10,39 +10,73 @@ import UIKit
 import CoreData
 class Cell: UITableViewCell {
     @IBOutlet var currencyTextLabel: UILabel!
-    
+    @IBOutlet var createdAtTextLabel: UILabel!
+    @IBOutlet var amountTextLabel: UILabel!
+    @IBOutlet var statusTextLabel: UILabel!
+    func setupWith(date:String)->String{
+
+        let dateFormatter = DateFormatter()
+        let dateDouble = Double(date)
+        let date = Date(timeIntervalSinceReferenceDate: dateDouble!)
+        
+        dateFormatter.setLocalizedDateFormatFromTemplate("MMM dd,YYYY hh:mm a")
+        dateFormatter.timeZone = NSTimeZone(abbreviation: "UTC") as TimeZone?
+        print(dateFormatter.string(from: date))
+   
+       return dateFormatter.string(from: date)
+        }
+    }
+extension Date {
+   func getFormattedDate(format: String) -> String {
+        let dateformat = DateFormatter()
+        dateformat.dateFormat = format
+        return dateformat.string(from: self)
+    }
 }
 class DisplayOrdersViewController : UIViewController, UITableViewDelegate {
     @IBOutlet var tableView: UITableView!
     var savedOrdersObject = [NSManagedObject]()
-
+    let vc = ViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.delegate = self
         self.tableView.dataSource = self
         fetchCoreData()
     }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        deleteCoreData()
+    }
     private func fetchCoreData(){
-        //1
         guard let appDelegate =
-          UIApplication.shared.delegate as? AppDelegate else {
-            return
+                UIApplication.shared.delegate as? AppDelegate else {
+            fatalError()
         }
+        let personFetchRequest: NSFetchRequest<SavedOrders> = SavedOrders.fetchRequest()
+        let context = appDelegate.persistentContainer.viewContext
+        let orders = try? context.fetch(personFetchRequest)
+        guard let order = orders else {fatalError()}
         
-        let managedContext =
-          appDelegate.persistentContainer.viewContext
-        
-        //2
-        let fetchRequest =
-          NSFetchRequest<NSManagedObject>(entityName: "SavedOrders")
-        
-        //3
-        do {
-            savedOrdersObject = try managedContext.fetch(fetchRequest)
+        context.perform {
             self.tableView.reloadData()
-        } catch let error as NSError {
-          print("Could not fetch. \(error), \(error.userInfo)")
         }
+        savedOrdersObject = order
+    }
+    func deleteCoreData(){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        for object in savedOrdersObject {
+            context.delete(object)
+        }
+        do {
+            try context.save()
+        } catch{
+            
+        }
+        
     }
 }
 
@@ -50,16 +84,22 @@ extension DisplayOrdersViewController : UITableViewDataSource {
     func tableView(_ tableView: UITableView,
                    numberOfRowsInSection section: Int) -> Int {
         print(savedOrdersObject.count)
-      return savedOrdersObject.count
+        return savedOrdersObject.count
     }
-
+    
     func tableView(_ tableView: UITableView,cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-      let order = savedOrdersObject[indexPath.row]
-      let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! Cell
+        
+        let order = savedOrdersObject[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! Cell
         cell.currencyTextLabel.text =
-        order.value(forKeyPath: "currency") as? String
-      return cell
+            order.value(forKeyPath: "currency") as? String
+        cell.amountTextLabel.text =
+            order.value(forKeyPath: "amount") as? String
+        cell.createdAtTextLabel.text = cell.setupWith(date: order.value(forKey: "createdAt") as? String ?? "na")
+        print(order.value(forKey: "createdAt") as? String)
+        cell.statusTextLabel.text =
+            order.value(forKeyPath: "orderStatus") as? String
+        return cell
     }
     
 }
--- Copper.co/Copper.co/OrderListDisplay/ViewController.swift
+++ Copper.co/Copper.co/OrderListDisplay/ViewController.swift
@@ -9,75 +9,64 @@ import UIKit
 import CoreData
 
 class ViewController: UIViewController {
-
+    
     var cachedOrders: [Orders] = []
     private var model : OrderListModel?
-    var managedObjectContext : NSManagedObjectContext?
     var savedOrdersObject: [NSManagedObject] = []
     let group = DispatchGroup()
-
     override func viewDidLoad() {
         super.viewDidLoad()
         model = OrderListModel()
         fetchData()
     }
-
+    
     private func fetchData(){
-        group.enter()
         model?.fetchData { [weak self] (cachedOrders,err) in
             guard let cachedOrders = cachedOrders else { return }
-            
             self?.loadData(order: cachedOrders)
         }
         
     }
     func loadData(order: [Orders]) {
         cachedOrders = order
+        
     }
-    func save(orders: [Orders]) {
-      
-      guard let appDelegate =
-        UIApplication.shared.delegate as? AppDelegate else {
-        return
-      }
-      
-      // Create context
-      let managedContext = appDelegate.persistentContainer.viewContext
-      
-      // Create entity
-      let entity = NSEntityDescription.entity(forEntityName: "SavedOrders", in: managedContext)!
-      
-      let savedObject = NSManagedObject(entity: entity, insertInto: managedContext)
-      
-      // Map object to saved object
-        for orders in orders {
-            for order in orders.orders {
-                savedObject.setValue(order.orderId, forKeyPath: "orderID")
-                savedObject.setValue(order.currency.rawValue, forKeyPath: "currency")
-                savedObject.setValue(order.amount, forKeyPath: "amount")
-                savedObject.setValue(order.orderType.rawValue, forKeyPath: "orderType")
-                savedObject.setValue(order.orderStatus.rawValue, forKeyPath: "orderStatus")
-                savedObject.setValue(order.createdAt, forKeyPath: "createdAt")
+    func writeLargeObject(orders:[Orders],completion: @escaping () -> Void) throws {
+        guard let appDelegate =
+                UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let context = appDelegate.persistentContainer.newBackgroundContext()
+        context.automaticallyMergesChangesFromParent = true
+        context.perform {
+            for orders in orders {
+                for order in orders.orders {
+                    let savedObject = SavedOrders(context: context)
+                    savedObject.currency = order.currency.rawValue
+                    savedObject.createdAt = order.createdAt
+                    savedObject.amount = order.amount
+                    savedObject.orderStatus = order.orderStatus.rawValue
+                }
+            }
+            do {
+                try context.save()
+                DispatchQueue.main.async {
+                    completion()
+                }
+            } catch {
+                
             }
         }
-      
-      // Save data to managedContext
-      do {
-        try managedContext.save()
-        self.savedOrdersObject.append(savedObject)
-        self.performSegue(withIdentifier: "show", sender: nil)
-      } catch let error as NSError {
-        print("Could not save. \(error), \(error.userInfo)")
-      }
     }
     @IBAction func download(_ sender: Any) {
-       save(orders: cachedOrders)
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let viewController = segue.destination as? DisplayOrdersViewController {
-            viewController.savedOrdersObject = savedOrdersObject
+        do {
+            try writeLargeObject(orders: cachedOrders,completion: {[weak self] in
+                
+            })
+        } catch {
+            print("Could not write the data")
         }
+        performSegue(withIdentifier: "show", sender: nil)
+        
     }
-    
 }
